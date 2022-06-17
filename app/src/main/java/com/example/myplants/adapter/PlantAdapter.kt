bc16df5df78d1ae02930b4a3bcc876f9a6f5f8a5
Benoit Chirazi <bchirazi@gmail.com>
2022-06-17 15:25:51 +0200

    plant list

--- app/src/main/java/com/example/myplants/adapter/PlantAdapter.kt
+++ app/src/main/java/com/example/myplants/adapter/PlantAdapter.kt
@@ -1,13 +1,21 @@
 package com.example.myplants.adapter
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.myplants.MainActivity
+import com.example.myplants.PlantModel
 import com.example.myplants.R
 
-class PlantAdapter: RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
+class PlantAdapter(
+    private val context: MainActivity,
+    private val plantList: List<PlantModel>,
+    private val layoutId: Int
+    ): RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
         val plantImage = view.findViewById<ImageView>(R.id.image_item)
     }
@@ -15,15 +23,18 @@ class PlantAdapter: RecyclerView.Adapter<PlantAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
        val view = LayoutInflater
            .from(parent.context)
-           .inflate(R.layout.item_horizontal_plant, parent, false)
+           .inflate(layoutId, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // recuperer les infos de la plante
+        val currentPlant = plantList[position]
+
+        // utiliser glidepour recuperer l'image a partir de son lien
+        Glide.with(context).load(Uri.parse(currentPlant.imageUrl)).into(holder.plantImage)
 
     }
 
-    override fun getItemCount(): Int {
-        return 5
-    }
+    override fun getItemCount(): Int = plantList.size
 }
--- app/src/main/java/com/example/myplants/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/myplants/fragments/HomeFragment.kt
@@ -6,10 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myplants.MainActivity
+import com.example.myplants.PlantModel
 import com.example.myplants.R
 import com.example.myplants.adapter.PlantAdapter
+import com.example.myplants.adapter.PlantItemDecoration
 
-class HomeFragment: Fragment() {
+class HomeFragment(
+    private val context: MainActivity
+): Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,8 +22,50 @@ class HomeFragment: Fragment() {
     ): View? {
         val view = inflater?.inflate(R.layout.fragment_home, container, false)
 
+        // creer une liste qui va stocker ces plantes
+        val plantList = arrayListOf<PlantModel>()
+
+        // enregister une 1ere plante
+        plantList.add(
+            PlantModel(
+            name = "Pissenlit",
+                description = "jaune soleil",
+                imageUrl = "https://cdn.pixabay.com/photo/2016/07/30/13/16/dandelion-1557110_1280.jpg",
+                liked = false
+        ))
+        // enregister une 2eme plante
+        plantList.add(
+            PlantModel(
+                name = "rose",
+                description = "ca pique un peu",
+                imageUrl = "https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529_1280.jpg",
+                liked = false
+            ))
+        // enregister une 3eme plante
+        plantList.add(
+            PlantModel(
+                name = "cactus",
+                description = "ca pique un bcp",
+                imageUrl = "https://media.istockphoto.com/photos/big-cactus-in-the-nature-picture-id1315231921?s=612x612",
+                liked = false
+            ))
+        // enregister une 4eme plante
+        plantList.add(
+            PlantModel(
+                name = "tulipe",
+                description = "c'est beau",
+                imageUrl = "https://cdn.pixabay.com/photo/2018/03/26/16/38/nature-3263198_1280.jpg",
+                liked = false
+            ))
+
+        // recuperer le recyclerview
         val horizontalRecyclerView = view.findViewById<RecyclerView>(R.id.horizontal_recycler_view)
-        horizontalRecyclerView.adapter = PlantAdapter()
+        horizontalRecyclerView.adapter = PlantAdapter(context, plantList, R.layout.item_horizontal_plant)
+
+        // recuperer le 2e recyclerview
+        val verticalrecyclerView = view.findViewById<RecyclerView>(R.id.vertical_recycler_view)
+        verticalrecyclerView.adapter = PlantAdapter(context, plantList, R.layout.item_vertical_plant)
+        verticalrecyclerView.addItemDecoration(PlantItemDecoration())
 
         return view
     }
